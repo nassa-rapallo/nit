13f64f384b7843b60b2daf7c1ef2fd6e942e1ba4
Giovanni Costa <cgiovanni1996@gmail.com>
2024-01-11 22:21:31 +0100

    :sparkles: initial first commit

---
+++ src/fonts/index.ts
@@ -0,0 +1,1 @@
+export { TTNorms } from "./tt-norms.font";
---
+++ src/fonts/tt-norms.font.tsx
@@ -0,0 +1,59 @@
+import localFont from "next/font/local";
+
+export const TTNorms = localFont({
+  src: [
+    {
+      path: "./tt-norms/extra-light.otf",
+      weight: "300",
+      style: "normal",
+    },
+
+    {
+      path: "./tt-norms/light.otf",
+      weight: "300",
+      style: "normal",
+    },
+
+    {
+      path: "./tt-norms/thin.otf",
+      weight: "300",
+      style: "normal",
+    },
+
+    {
+      path: "./tt-norms/regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+
+    {
+      path: "./tt-norms/medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+
+    {
+      path: "./tt-norms/bold.otf",
+      weight: "600",
+      style: "normal",
+    },
+
+    {
+      path: "./tt-norms/extra-bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+
+    {
+      path: "./tt-norms/black.otf",
+      weight: "800",
+      style: "normal",
+    },
+
+    {
+      path: "./tt-norms/extra-black.otf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
