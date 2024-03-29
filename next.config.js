f48c0af86484f7eae8a44c3e00112fdcc7ba88bc
Giovanni Costa <cgiovanni1996@gmail.com>
2024-03-29 17:49:37 +0100

    :sparkles: config files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withMDX = require("@next/mdx")();
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
   images: {
     remotePatterns: [
       {
@@ -12,4 +18,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig);
